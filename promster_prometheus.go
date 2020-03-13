9ee0fff7d4732d6fac7eb71806bef690330aa226
Gustavo Coelho <gutorc@hotmail.com>
2020-03-13 19:21:14 -0300

    Writing prometheus config
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- promster_prometheus.go
+++ promster_prometheus.go
@@ -6,7 +6,7 @@ import (
 	"os"
 
 	"github.com/prometheus/prometheus/config"
-	"gopkg.in/yaml.v1"
+	"gopkg.in/yaml.v2"
 )
 
 type PrometheusConfig config.Config
@@ -25,8 +25,9 @@ func (cfg *PrometheusConfig) RegisterFlags(f *flag.FlagSet) {
 	)
 	log.Infof("Testando")
 	log.Debugf("Config scrape interval %s", cfg.GlobalConfig.ScrapeInterval)
-	flag.Var(&cfg.GlobalConfig.ScrapeInterval, "scrape-interval", "Prometheus scrape interval")
-	flag.Var(&cfg.GlobalConfig.ScrapeTimeout, "scrape-timeout", "Prometheus scrape timeout")
+	flag.Var(&cfg.GlobalConfig.ScrapeInterval, "global-scrape-interval", "Prometheus scrape interval")
+	flag.Var(&cfg.GlobalConfig.ScrapeTimeout, "global-scrape-timeout", "Prometheus scrape timeout")
+	flag.Var(&cfg.GlobalConfig.EvaluationInterval, "global-evaluation-interval", "Prometheus global evaluation interval how frequently to evaluate rules")
 	flag.StringVar(&scrapeMatch, "scrape-match", "", "Metrics regex filter applied on scraped targets. Commonly used in conjunction with /federate metrics endpoint")
 	flag.BoolVar(&scrapeShardingEnable, "scrape-shard-enable", false, "Enable sharding distribution among targets so that each Promster instance will scrape a different set of targets, enabling distribution of load among instances. Defaults to true.")
 	flag.StringVar(&evaluationInterval, "evaluation-interval", "30s", "Prometheus evaluation interval")
@@ -34,12 +35,26 @@ func (cfg *PrometheusConfig) RegisterFlags(f *flag.FlagSet) {
 
 }
 
-func (cfg *PrometheusConfig) PrintConfig() {
-	d, err := yaml.Marshal(&cfg.GlobalConfig)
+func (cfg *PrometheusConfig) String() string {
+	b, err := yaml.Marshal(cfg)
 	if err != nil {
-		log.Fatalf("error: %v", err)
+		return fmt.Sprintf("<error creating config string: %s>", err)
 	}
-	fmt.Printf("--- m dump:\n%s\n\n", string(d))
+	return string(b)
+}
+
+func (cfg *PrometheusConfig) PrintConfig() {
+	// d, err := yaml.Marshal(&cfg.GlobalConfig)
+	// if err != nil {
+	// 	log.Fatalf("error: %v", err)
+	// }
+	var scrape config.ScrapeConfig
+	scrape.JobName = "prometheus"
+	// scrape.Params.Set("teste", "localhost:9090")
+	// scrape.ServiceDiscoveryConfig.StaticConfigs.Targets.LabelSet
+	cfg.ScrapeConfigs = append(cfg.ScrapeConfigs, &scrape)
+	d := cfg.String()
+	fmt.Printf("--- m dump:\n%s\n\n", d)
 	f, err := os.Create("prometheus_test.yml")
 	if err != nil {
 		log.Fatalf("error: %v", err)
